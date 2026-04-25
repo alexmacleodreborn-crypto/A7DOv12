97929e42c6f12f2d2b7e98d830a7424cf7080abe
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-04-25 23:05:41 +0100

    Update A7DO_Brain.py

--- core/A7DO_Brain.py
+++ core/A7DO_Brain.py
@@ -6,10 +6,11 @@
 import time
 
 class A7DO_Brain:
-    def __init__(self, skeleton_manifold, muscular_manifold, articulation_manifold, maturation_engine):
+    def __init__(self, skeleton_manifold, muscular_manifold, articulation_manifold, kinematic_engine, maturation_engine):
         self.skeleton = skeleton_manifold
         self.muscles = muscular_manifold
         self.joints = articulation_manifold
+        self.kinematics = kinematic_engine
         self.growth = maturation_engine
         
         self.pulse_count = 0
@@ -34,6 +35,13 @@ class A7DO_Brain:
             bone = self.skeleton.registry.get(bone_id)
             if bone: bone.dimensions["length"] = original_len * limb_ratio
 
+    def set_muscle_tension(self, muscle_id, tension_value):
+        """Allows external systems to command muscle contraction."""
+        muscle = self.muscles.registry.get(muscle_id)
+        if muscle:
+            # Clamp tension strictly between 0.0 and 1.0
+            muscle.tension = max(0.0, min(1.0, tension_value))
+
     def execute_system_pulse(self):
         self.pulse_count += 1
         
@@ -44,21 +52,27 @@ class A7DO_Brain:
         # 2. PROPORTIONAL OVERRIDE
         self._apply_da_vinci_ratios(growth_stats["head_ratio"], growth_stats["limb_ratio"])
         
-        # 3. SKELETAL 3D CALCULATION
+        # 3. KINEMATIC ROTATION (NEW!)
+        # Translates muscle tension into bone rotation angles
+        self.kinematics.apply_kinematics(self.muscles.registry, self.skeleton.registry)
+        
+        # 4. SKELETAL 3D CALCULATION
+        # Physically calculates coordinates using the new rotated angles
         self.skeleton.generate_current_geometry(growth_stats["scale_x"])
         
-        # 4. JOINT 3D CALCULATION (New!)
+        # 5. JOINT 3D CALCULATION
         self.joints.generate_current_joints(self.skeleton.registry)
         
-        # 5. MUSCULAR VECTOR CALCULATION
+        # 6. MUSCULAR VECTOR CALCULATION
+        # Muscles stretch or compress to match the newly moved bones
         self.muscles.generate_current_musculature(self.skeleton.registry)
         
         self.system_status = "SYNCHRONIZED"
         return self.export_unified_state(growth_stats)
 
     def export_unified_state(self, growth_stats):
-        bone_data = {b_id: {"center": b.pos_center, "proximal": b.pos_proximal, "distal": b.pos_distal} for b_id, b in self.skeleton.registry.items()}
-        muscle_data = {m_id: {"origin": m.pos_origin_3d, "insertion": m.pos_insertion_3d, "length": m.current_length} for m_id, m in self.muscles.registry.items()}
+        bone_data = {b_id: {"center": b.pos_center, "proximal": b.pos_proximal, "distal": b.pos_distal, "rotation": b.rotation} for b_id, b in self.skeleton.registry.items()}
+        muscle_data = {m_id: {"origin": m.pos_origin_3d, "insertion": m.pos_insertion_3d, "length": m.current_length, "tension": m.tension} for m_id, m in self.muscles.registry.items()}
         joint_data = {j_id: {"pos": j.pos_3d, "type": j.joint_type} for j_id, j in self.joints.registry.items()}
             
         return {
